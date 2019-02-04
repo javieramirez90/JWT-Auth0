c73d8f4871133b7df4719d7a371ddd2e36d647e0
javieramirez90 <javier.ramirez@aiesec.net>
2019-02-04 15:13:17 -0600

    Json Web Token & Auth0 egghead course

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8888;
+
+app.get('/status', (req, res) => {
+  const localTime = (new Date()).toLocaleTimeString();
+  console.log(localTime)
+
+  res
+  .status(200)
+  .send(`Server time is  ${localTime}.`);
+});
+
+app.get("*", (req, res) => {
+  res.sendStatus(404);
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
